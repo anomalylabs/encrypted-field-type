eee6bfd32f4c8d3c553d07898973ff4c7b42a8cc
Ryan Thompson <ryan@pyrocms.com>
2016-02-18 01:00:57 -0600

    Author

--- src/EncryptedFieldTypePresenter.php
+++ src/EncryptedFieldTypePresenter.php
@@ -6,9 +6,9 @@ use Illuminate\Encryption\Encrypter;
 /**
  * Class EncryptedFieldTypePresenter
  *
- * @link          http://anomaly.is/streams-platform
- * @author        AnomalyLabs, Inc. <hello@anomaly.is>
- * @author        Ryan Thompson <ryan@anomaly.is>
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
  * @package       Anomaly\EncryptedFieldType
  */
 class EncryptedFieldTypePresenter extends FieldTypePresenter
