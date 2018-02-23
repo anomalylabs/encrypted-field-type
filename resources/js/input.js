1c0b2e4039a0a50f2c389d6a66c92bf2f08e9944
Ryan Thompson <ryan@pyrocms.com>
2018-02-23 13:27:01 -0600

    Update input.js

--- resources/js/input.js
+++ resources/js/input.js
@@ -8,8 +8,10 @@
 
         field.parentElement.querySelector('[data-toggle="text"]').addEventListener('click', function (event) {
 
-            event.target.querySelector('i').classList.toggle('fa-toggle-on');
-            event.target.querySelector('i').classList.toggle('fa-toggle-off');
+            let indicator = event.target.querySelector('i');
+            
+            indicator.classList.toggle('fa-toggle-on');
+            indicator.classList.toggle('fa-toggle-off');
 
             if (field.getAttribute('type') === 'password') {
                 field.setAttribute('type', 'text');
